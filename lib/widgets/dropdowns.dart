b3034b1ab4e288a5747a728e4bcea04fc928feb5
blvcksamurai <theblvcksamurai@gmail.com>
2024-03-06 07:45:43 +0100

    Drop Down Widget initialisation

--- lib/widgets/dropdowns.dart
+++ lib/widgets/dropdowns.dart
@@ -1,34 +1,34 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class DropDowns extends StatelessWidget {
-//   String text;
-//   DropDowns({
-//     required this.text,
-//     super.key,
-//   });
+class DropDowns extends StatelessWidget {
+  String text;
+  DropDowns({
+    required this.text,
+    super.key,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [
-//         Text(
-//           text,
-//           style: TextStyle(
-//             color: Color(0xFFBDBDC1),
-//             fontSize: 10,
-//             fontFamily: 'Monument',
-//             fontWeight: FontWeight.w400,
-//             letterSpacing: 0.20,
-//           ),
-//         ),
-//         IconButton(
-//           onPressed: () {},
-//           icon: Icon(
-//             Icons.arrow_drop_down_rounded,
-//             color: Color(0xFF6D6661),
-//           ),
-//         )
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            color: Color(0xFFBDBDC1),
+            fontSize: 10,
+            fontFamily: 'Monument',
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.20,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_drop_down_rounded,
+            color: Color(0xFF6D6661),
+          ),
+        )
+      ],
+    );
+  }
+}
