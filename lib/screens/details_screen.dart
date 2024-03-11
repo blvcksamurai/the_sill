f3edcb34d96e4001c87186f2fe048bcd7c424ad3
blvcksamurai <theblvcksamurai@gmail.com>
2024-03-11 15:11:34 +0100

    Detail screen view

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -11,53 +11,54 @@ class DetailsScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Container(
-        child: Stack(
-      children: [
-        Container(
-          height: size.height,
-          width: size.width,
-        ),
-        Container(
-          width: size.width,
-          height: size.height * 0.70,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/haworthia.png'),
-              fit: BoxFit.fill,
-            ),
-          ),
-        ),
-        Positioned(
-          top: size.height * 0.63,
-          child: Stack(
-            children: [
-              Container(
-                width: size.width,
-                height: size.height * 0.40,
-                decoration: ShapeDecoration(
-                  color: Color(0xff5b5551),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40),
-                      bottomLeft: Radius.circular(0),
-                      bottomRight: Radius.circular(0),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: size.width,
-                height: size.height * 0.30,
-                child: Image(
-                  image: AssetImage('assets/images/leaf.png'),
-                  fit: BoxFit.fill,
-                ),
-              )
-            ],
-          ),
-        ),
-      ],
-    ));
+        //     child: Stack(
+        //   children: [
+        //     Container(
+        //       height: size.height,
+        //       width: size.width,
+        //     ),
+        //     Container(
+        //       width: size.width,
+        //       height: size.height * 0.70,
+        //       decoration: BoxDecoration(
+        //         image: DecorationImage(
+        //           image: AssetImage('assets/images/haworthia.png'),
+        //           fit: BoxFit.fill,
+        //         ),
+        //       ),
+        //     ),
+        //     Positioned(
+        //       top: size.height * 0.63,
+        //       child: Stack(
+        //         children: [
+        //           Container(
+        //             width: size.width,
+        //             height: size.height * 0.40,
+        //             decoration: ShapeDecoration(
+        //               color: Color(0xff5b5551),
+        //               shape: RoundedRectangleBorder(
+        //                 borderRadius: BorderRadius.only(
+        //                   topLeft: Radius.circular(40),
+        //                   topRight: Radius.circular(40),
+        //                   bottomLeft: Radius.circular(0),
+        //                   bottomRight: Radius.circular(0),
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //           Container(
+        //             width: size.width,
+        //             height: size.height * 0.30,
+        //             child: Image(
+        //               image: AssetImage('assets/images/leaf.png'),
+        //               fit: BoxFit.fill,
+        //             ),
+        //           )
+        //         ],
+        //       ),
+        //     ),
+        //   ],
+        // )
+        );
   }
 }
